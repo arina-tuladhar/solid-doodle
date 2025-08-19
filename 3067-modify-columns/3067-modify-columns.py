b8e01a4459d0f5f904e619b1e4345c7e504d44ec
Arina Tuladhar <tuladhararina@gmail.com>
2025-08-19 09:54:38 +0545

    Time: 269 ms (69.22%), Space: 66.4 MB (24.79%) - LeetHub

---
+++ 3067-modify-columns/3067-modify-columns.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def modifySalaryColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    employees['salary'] = employees['salary'] * 2
+    return employees
+
+data = {'name':['Jack', 'Piper', 'Mia', 'Ulysses'], 'salary': [19666, 74754, 62509, 54866]}
+df = pd.DataFrame(data)
+
+print(modifySalaryColumn(data))

3ac874f16c07ff63e360abd88d3f2651ca3fb934
Arina Tuladhar <tuladhararina@gmail.com>
2025-08-20 08:48:34 +0545

    Update 3063-method-chaining.py

--- 3063-method-chaining/3063-method-chaining.py
+++ 3063-method-chaining/3063-method-chaining.py
@@ -1,8 +1,13 @@
 import pandas as pd
 
 def findHeavyAnimals(animals: pd.DataFrame) -> pd.DataFrame:
+        # Filter animals with weight > 100
     filtered = animals[animals['weight'] > 100]
+
+    # Sort by weight in descending order
     sorted_animals = filtered.sort_values(by='weight', ascending=False)
+
+    # Return only the name column
     return sorted_animals[['name']]
 
 data = {"name": ["Tatiana", "Khaled", "Alex", "Jonathan", "Stefan", "Tommy"],
